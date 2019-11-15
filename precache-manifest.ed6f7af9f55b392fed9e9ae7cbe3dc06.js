ebf0b6a825b3d891ed492ba51976c1e3d32a7ef5
Pavel Sturov <paul.sturov@gmail.com>
2019-11-15 18:42:19 +0300

    Updates

---
+++ precache-manifest.ed6f7af9f55b392fed9e9ae7cbe3dc06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37d6cb16226fa88cae9761325dbad8d",
+    "url": "/react-apps/todo-list/index.html"
+  },
+  {
+    "revision": "10f80d3ee374a91e170d",
+    "url": "/react-apps/todo-list/static/css/main.ee62991a.chunk.css"
+  },
+  {
+    "revision": "a8c813652119a1e5e8aa",
+    "url": "/react-apps/todo-list/static/js/2.91bcc874.chunk.js"
+  },
+  {
+    "revision": "10f80d3ee374a91e170d",
+    "url": "/react-apps/todo-list/static/js/main.9aedbd59.chunk.js"
+  },
+  {
+    "revision": "85303920ece6154593ef",
+    "url": "/react-apps/todo-list/static/js/runtime-main.7eadcff5.js"
+  }
+]);
